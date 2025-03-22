559eee3714ca742eb8f1c9fba2b93d18c0660a32
tbhawsar <tilakbhavsar64@gmail.com>
2025-03-22 07:39:29 +0000

    Replaced layoutbuilder() with MediaQuery.of(context)

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -8,72 +8,70 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    double screenWidth = mediaQuery.size.width;
     return Scaffold(
       body: Center(
         child: SingleChildScrollView(
-          child: LayoutBuilder(
-            builder: (context, BoxConstraints constraints) {
-              return FractionallySizedBox(
-                widthFactor: constraints.maxWidth > 500 ? 0.5 : 1.0,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Lottie.asset(
-                        'assets/lotties/loading_icon.json',
-                        height: 200.0,
-                      ),
-                      FittedBox(
-                        child: Text(
-                          'Simple List',
-                          style: TextStyle(
-                            color: Colors.teal,
-                            fontSize: 30.0,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 10.0,
-                          ),
-                        ),
-                      ),
-                      FilledButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return LoginPage(
-                                  title: 'Login',
-                                );
-                              },
-                            ),
-                          );
-                        },
-                        style: FilledButton.styleFrom(
-                          minimumSize: Size(double.infinity, 40.0),
-                        ),
-                        child: Text('Login'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return OnboardingPage();
-                              },
-                            ),
-                          );
-                        },
-                        style: FilledButton.styleFrom(
-                          minimumSize: Size(double.infinity, 40.0),
-                        ),
-                        child: Text('Register'),
-                      ),
-                    ],
+          child: FractionallySizedBox(
+            widthFactor: screenWidth > 500 ? 0.5 : 1.0,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Lottie.asset(
+                    'assets/lotties/loading_icon.json',
+                    height: 200.0,
                   ),
-                ),
-              );
-            },
+                  FittedBox(
+                    child: Text(
+                      'Simple List',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 10.0,
+                      ),
+                    ),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return LoginPage(
+                              title: 'Login',
+                            );
+                          },
+                        ),
+                      );
+                    },
+                    style: FilledButton.styleFrom(
+                      minimumSize: Size(double.infinity, 40.0),
+                    ),
+                    child: Text('Login'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return OnboardingPage();
+                          },
+                        ),
+                      );
+                    },
+                    style: FilledButton.styleFrom(
+                      minimumSize: Size(double.infinity, 40.0),
+                    ),
+                    child: Text('Register'),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
