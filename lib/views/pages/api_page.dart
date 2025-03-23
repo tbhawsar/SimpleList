f306b166559a6e448e5cf255404341a45bfb53cf
tbhawsar <tilakbhavsar64@gmail.com>
2025-03-23 16:23:43 +0000

    successfully implemented API

--- lib/views/pages/api_page.dart
+++ lib/views/pages/api_page.dart
@@ -13,50 +13,60 @@ class ApiPage extends StatefulWidget {
 }
 
 class _ApiPageState extends State<ApiPage> {
-  late Activity activity;
   @override
   void initState() {
     getData();
     super.initState();
   }
 
-  void getData() async {
+  Future getData() async {
     var url = Uri.https('bored-api.appbrewery.com', '/random');
     var response = await http.get(url);
     if (response.statusCode == 200) {
-      activity = Activity.fromJson(
+      return Activity.fromJson(
           convert.jsonDecode(response.body) as Map<String, dynamic>);
     } else {
-      throw Exception('Failed to load album');
+      throw Exception('Failed to load activity');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              HeroWidget(
-                title: 'API Page',
-              ),
-              FilledButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) {
-                        return WidgetTree();
-                      }),
-                    );
-                  },
-                  child: Text('Exit'))
-            ],
-          ),
-        ),
-      ),
-    );
+        appBar: AppBar(),
+        body: FutureBuilder(
+          future: getData(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasData) {
+              Activity activity = snapshot.data;
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      HeroWidget(
+                        title: activity.activity,
+                      ),
+                      FilledButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) {
+                                return WidgetTree();
+                              }),
+                            );
+                          },
+                          child: Text('Exit'))
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return Center(child: Text('Error'));
+            }
+          },
+        ));
   }
 }
